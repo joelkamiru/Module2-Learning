4a254f02774b07d810279051c666eba669286ae6
Nancy Adelice Umutoniwase <nancytoniw@gmail.com>
2026-03-30 17:09:09 +0200

    New changes

---
+++ index.js
@@ -0,0 +1,31 @@
+// Javascript basics recap
+
+//Q2
+console.log("Hello Javascript");
+console.log("I am practicing JS basics");
+console.log("Today i am learning operators");
+//Q3 and Q4
+// const, let, var
+var studentName= "Nancy";
+let age = 26;
+const school="Moringa";
+let isStudent= true;
+
+//Q5
+console.log(studentName);
+
+console.log(school);
+console.log(isStudent);
+
+//Q6
+age="&";
+console.log(age);
+//Q7
+
+console.log (typeof studentName);
+console.log (typeof age);
+console.log (typeof isStudent);
+console.log (typeof school);
+
+//Q8
+let favoriteFoods =["smotcha","Githeri","Chapo"];
---
+++ string.js
@@ -0,0 +1,23 @@
+let firstName="NAncy";
+let lastName="Umutoniwase";
+//concatenation
+console.log(firstName + " "+ lastName)
+
+let age=34;
+// console.log("My name is " + firstName + " and I am " +age+" years old ")
+// backticks
+console.log(`My name is ${firstName} and i am ${age} years old`);
+// checking the length
+let word="Javascript";
+console.log(word.length);
+// accessing a character from a string using index position starting from 0
+let x="Hello";
+console.log(x[2]);
+console.log(x[4]);
+console.log(x.length);
+console.log(word[4]);
+
+console.log(word.toUpperCase())
+console.log(word.toLowerCase())
+
+
